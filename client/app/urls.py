25fbf058c8cc04355bb0dfbe0a1876d0438c8c34
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-05-02 01:10:30 -0500

    guardar archivos

--- client/app/urls.py
+++ client/app/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
+from .views import *
 
 urlpatterns = [
-
+    path('loguin/', LoguinView.as_view(), name='loguin'),
+    path('carpetas/', FolderView.as_view(), name='carpetas'),
+    re_path(r'^carpetas/(?P<folderId>\d+)/$', FolderView.as_view(), name='carpetasPadre'),
+    path('archivos/', FileView.as_view(), name='archivos'),
 ]
--- client/app/views.py
+++ client/app/views.py
@@ -4,31 +4,40 @@ from rest_framework.response import Response
 import requests
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.views.decorators.http import require_POST
 from django.conf import settings
+from django.http import JsonResponse
 import jwt
 from zeep import Client
 import base64
+from .serializers import *
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+import datetime
+import os
+from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser
 
-from client.app.serializers import *
 
 clientUser = Client('http://172.171.240.20:80/users/soap/?wsdl')
 clientFolder = Client('http://172.171.240.20:80/server/soap/?wsdl')
 clientFile= Client('http://172.171.240.20:80/files/soap/?wsdl')
 
 #region metodos de usuarios
+class LoguinView(APIView):
+    
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data.get('username', None)
+        password = data.get('password', None)
+        serializers = UserCredentialsSerializer(data={'username': username, 'password': password})
+        
+        if serializers.is_valid():
+            username = serializers.validated_data.get('username')
+            password = serializers.validated_data.get('password')
+            response = clientUser.service.loginSoap(username,password)
 
-def loginView (username, password):
-    
-    serializers = UserCredentialsSerializer(data={'username': username, 'password': password})
-    
-    if serializers.is_valid():
-        
-        username = serializers.validated_data.get('username')
-        password = serializers.validated_data.get('password')
-        
-        response = clientUser.service.loginSoap(username,password)
-        
-        return response
+
+            return Response({'token':response})
     
 def registerView (username, email, name, age, password):
     
@@ -52,34 +61,70 @@ def registerView (username, email, name, age, password):
 
 class FolderView(APIView):
     
-    def get(self, request):
+    def get(self, request, folderId):
         
-        token = request.data.get('token')
+        token = request.headers.get('Authorization')
         
-        folders = clientFolder.service.get_folders_by_parent_id(token,0)
+        folders = clientFolder.service.get_folders_by_parent_id(token,folderId)
         
-        return Response(folders)
+        data = []
+        
+        if folders != None:
+            for item in folders:
+                if item['type'] == 'folder':
+                    storage_value = 0 if item['storage'] is None else item['storage']
+                    
+                    folder_data = {
+                        'id': item['id'],
+                        'folderName': item['folderName'],
+                        'parentFolder': item['parentFolder'],
+                        'userId': item['userId'],
+                        'type': item['type'],
+                        'storage': storage_value,
+                    }
+                    data.append(folder_data)
+                elif item['type'] == 'file':
+                    parent = 0 if item['parentFolder'] is None else item['parentFolder']
+                    file_data = {
+                        'id': item['id'],
+                        'fileName': item['fileName'],
+                        'parentFolder': parent,
+                        'userId': item['userId'],
+                        'type': item['type'],
+                        'extension': item['extension'],
+                    }
+                    data.append(file_data)
+        else:
+            data = []
+        return Response({'data': data,})
     
     def post(self, request):
         
         folderName = request.data.get('folderName')
-        token = request.data.get('token')
+        token = request.headers.get('Authorization')
         folderParent = request.data.get('folderParent')
         
+        print(request.data)
         agregar = clientFolder.service.registerFolderSoap(token,folderName,folderParent)
         
-        return Response(agregar)
+        if agregar != None:
+            return Response('exito')
+        else:
+            return Response('mal')
     
     def put(self, request):
         
         folderName = request.data.get('folderName')
-        token = request.data.get('token')
+        token = request.headers.get('Authorization')
         folderId = request.data.get('folderId')
         folderParent = request.data.get('folderParent')
-        
+
         editar = clientFolder.service.updateFolderSoap(token, folderId, folderName, folderParent)
         
-        return Response(editar)
+        if editar != None:
+            return Response('exito')
+        else:
+            return Response('mal')
     
     def delete(self, request):
         
@@ -89,5 +134,53 @@ class FolderView(APIView):
         eliminar = clientFolder.service.deleteFolderSoap(token, folderId)
         
         return Response(eliminar)
-        
+
+def convert_to_serializable(self, obj):
+        if isinstance(obj, dict):
+            return {k: self.convert_to_serializable(v) for k, v in obj.items()}
+        elif isinstance(obj, (list, tuple)):
+            return [self.convert_to_serializable(i) for i in obj]
+        elif hasattr(obj, '__dict__'):
+            return self.convert_to_serializable(obj.__dict__)
+        elif isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+        else:
+            return obj
 #endregion
+
+#region metodos archivo
+
+def encode_image(uploaded_file):
+    encoded_image = base64.b64encode(uploaded_file.read()).decode('utf-8')
+    return encoded_image
+
+def get_filename(image_path):
+    return os.path.basename(image_path)
+
+def get_filesize(image_path):
+    return os.path.getsize(image_path)
+
+class FileView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+    
+    def post(self,request,*args,**kwargs):
+        token = request.headers.get('Authorization')
+        folder_id = request.data.get('folder_id')
+        files = request.FILES.getlist('files')
+        print(files)
+        files_info = []
+        
+        for image_path in files:
+            filename = image_path.name
+            filesize = image_path.size
+            encoded_image = encode_image(image_path)
+            file_sha256 = 'hola'
+            files_info.append({"filename": filename, "filesize": filesize, "encoded_image": encoded_image, "file_hash": file_sha256})
+
+        for file_info in files_info:
+            response = clientFile.service.process_file(token, file_info["filename"], file_info["filesize"], file_info["encoded_image"], file_info["file_hash"], folder_id)
+
+        return Response('yes')
+#endregion
+
+
